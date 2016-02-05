de7b7b4568e0b1017436d0a3e2b322e31643e6d4
tnog2014 <tnog@example.com>
2016-02-05 16:30:57 +0900

    最初のコミット

---
+++ JavaPractice/src/practice01_q/ReportGenerator.java
@@ -0,0 +1,25 @@
+package practice01_q;
+
+import java.io.IOException;
+
+/**
+ * レポート作成クラス.
+ *
+ * CSVファイルを読み込んで、顧客ＩＤごとの利用金額合計を計算する。
+ * レポートファイルに、顧客ＩＤと利用金額の一覧を表示する。尚、顧客ＩＤはアルファベット順にソートした状態とする。
+ *
+ * @author tnoguchi
+ *
+ */
+public class ReportGenerator {
+
+	/**
+	 * レポートを作成する.
+	 *
+	 * @param inputCsvPath 入力ファイルパス
+	 * @param reportFilePath レポートファイルパス
+	 */
+	public void create(String inputCsvPath, String reportFilePath) throws IOException {
+
+	}
+}
